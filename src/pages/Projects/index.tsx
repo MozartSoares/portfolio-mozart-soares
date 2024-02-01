515e228699649a521685a3f81694d785f55d9d15
Mozart Soares <mozartarmstrong@gmail.com>
2024-01-31 22:04:02 -0300

    --fix api agora puxa preview dos projetos pelo assets do release

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -23,27 +23,26 @@ const Projects = () => {
           (a, b) => b.stargazers_count - a.stargazers_count
         )
         const fetchImageUrl = async (repo) => {
-          if (repo.open_issues_count > 0) {
-            return fetch(
-              `https://api.github.com/repos/${repo.full_name}/issues/1/comments`,
-              {
-                headers: headers
+          try {
+            const releasesUrl = `https://api.github.com/repos/${repo.full_name}/releases/latest`
+            const releasesResponse = await fetch(releasesUrl, { headers })
+            const releaseJson = await releasesResponse.json()
+
+            if (releaseJson.assets.length > 0) {
+              const imageUrl = releaseJson.assets[0].browser_download_url
+              return {
+                ...repo,
+                imageUrl: imageUrl
               }
+            } else {
+              return repo
+            }
+          } catch (error) {
+            console.error(
+              `Error fetching image URL for ${repo.full_name}:`,
+              error
             )
-              .then((res) => res.json())
-              .then((resJson) => {
-                const imageUrl = resJson[0].body
-                if (imageUrl) {
-                  return {
-                    ...repo,
-                    imageUrl: imageUrl
-                  }
-                } else {
-                  return repo
-                }
-              })
-          } else {
-            return Promise.resolve(repo)
+            return repo
           }
         }
         const promises = sortedReposByStars.map((repo) => fetchImageUrl(repo))
